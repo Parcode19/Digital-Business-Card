cc3689a27108466c639225cf7e924e6922338d05
Parth <parthp15@yahoo.com>
2023-09-03 19:51:00 -0700

    added about.jsx

--- vite-react/src/components/About.jsx
+++ vite-react/src/components/About.jsx
@@ -1,7 +1,8 @@
 export default function About() {
     return (
         <div className="about--container"> 
-
+            <h1>Parth Patel</h1>
+            <h3>Full Stack Developer</h3>
         </div>
     )
 }
