9ab3085e4f7d9d7e99c2405fe2a11296c1790525
Parth <parthp15@yahoo.com>
2023-09-04 22:42:02 -0700

    number can be copied now

--- vite-react/src/components/Contact.jsx
+++ vite-react/src/components/Contact.jsx
@@ -1,41 +1,55 @@
-import github from "../assets/github (3).png"
-import linkedin from "../assets/linkedin (2).png"
-import mail from "../assets/gmail (1).png"
-import call from "../assets/emergency-call.png"
-import insta from "../assets/instagram (1).png"
+import { useState } from 'react';
+import github from "../assets/github (3).png";
+import linkedin from "../assets/linkedin (2).png";
+import mail from "../assets/gmail (1).png";
+import call from "../assets/emergency-call.png";
+import insta from "../assets/instagram (1).png";
 
 export default function Contact() {
     const githubLink = "https://github.com/Parcode19";
     const linkedinLink = "https://linkedin.com/in/parth-patel-433703266";
     const instagram = "https://www.instagram.com/";
     const number = "+1 (236) 995-2972";
-    const gmail = "parthrp15@gmail.com"
+    const gmail = "parthrp15@gmail.com";
+
+    const [isCopied, setIsCopied] = useState(false);
+
+    const handleCopyNumber = () => {
+        const hiddenInput = document.createElement('input');
+        document.body.appendChild(hiddenInput);
+        hiddenInput.value = number;
+        hiddenInput.select();
+        document.execCommand('copy');
+        document.body.removeChild(hiddenInput);
+        setIsCopied(true);
+    };
 
     return (
         <div className="contact--container"> 
             <div className="contact--content">
                 <a href={githubLink} className="github-icon">
-                    <img src={github} width="25px"></img>
+                    <img src={github} width="25px" alt="GitHub"></img>
                 </a> 
             </div>
             <div className="contact--content">
-                <a href={linkedinLink}  className="linkedin-icon">
-                    <img src={linkedin} width="25px"></img>
+                <a href={linkedinLink} className="linkedin-icon">
+                    <img src={linkedin} width="25px" alt="LinkedIn"></img>
                 </a> 
             </div>
             <div className="contact--content">
-                <a href={gmail}  className="mail-icon">
-                    <img src={mail} width="25px"></img>
+                <a href={`mailto:${gmail}`} className="mail-icon">
+                    <img src={mail} width="25px" alt="Email"></img>
                 </a> 
             </div>
             <div className="contact--content">
-                <a href={number}  className="call-icon">
-                    <img src={call} width="25px"></img>
+                <a href="#" className="call-icon" onClick={handleCopyNumber}>
+                    <img src={call} width="25px" alt="Copy Phone Number"></img>
                 </a> 
+                {isCopied && <span style={{ marginLeft: '5px' }}>Copied!</span>}
             </div>
             <div className="contact--content">
-                <a href={instagram}  className="insta-icon">
-                    <img src={insta} width="25px"></img> 
+                <a href={instagram} className="insta-icon">
+                    <img src={insta} width="25px" alt="Instagram"></img>
                 </a> 
             </div>
         </div>
