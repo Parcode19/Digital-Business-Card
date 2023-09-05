d9d45740a428a0505146d0ea61c2798d4fed7725
Parth <parthp15@yahoo.com>
2023-09-04 22:32:14 -0700

    contact done

--- vite-react/src/components/Contact.jsx
+++ vite-react/src/components/Contact.jsx
@@ -1,51 +1,43 @@
-import github from "../assets/github.png"
-import linkedin from "../assets/linkedin.png"
-
-// export default function Contact() {
-//     return (
-//         <div className="contact--container"> 
-//             <ul>
-//                 <li>
-//                     <span className="github-icons">
-//                         <img src={github} width="25px"></img>
-//                     </span> 
-//                 </li>
-//                 <li>
-//                     <span className="linkedin-icons">
-//                         <img src={linkedin} width="25px"></img>
-//                     </span> 
-//                 </li>
-//             </ul>
-//         </div>
-//     )
-// }
+import github from "../assets/github (3).png"
+import linkedin from "../assets/linkedin (2).png"
+import mail from "../assets/gmail (1).png"
+import call from "../assets/emergency-call.png"
+import insta from "../assets/instagram (1).png"
 
 export default function Contact() {
-    // Define the URLs for the respective links
     const githubLink = "https://github.com/Parcode19";
     const linkedinLink = "https://linkedin.com/in/parth-patel-433703266";
+    const instagram = "https://www.instagram.com/";
+    const number = "+1 (236) 995-2972";
+    const gmail = "parthrp15@gmail.com"
 
     return (
-        <div className="contact--container">
-            <ul>
-                <li>
-                    <a href={githubLink} target="_blank" rel="noopener noreferrer">
-                        <span className="github-icons">
-                            <img src={github} width="25px" alt="GitHub"></img>
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href={linkedinLink} target="_blank" rel="noopener noreferrer">
-                        <span className="linkedin-icons">
-                            <img src={linkedin} width="25px" alt="LinkedIn"></img>
-                        </span>
-                    </a>
-                </li>
-            </ul>
+        <div className="contact--container"> 
+            <div className="contact--content">
+                <a href={githubLink} className="github-icon">
+                    <img src={github} width="25px"></img>
+                </a> 
+            </div>
+            <div className="contact--content">
+                <a href={linkedinLink}  className="linkedin-icon">
+                    <img src={linkedin} width="25px"></img>
+                </a> 
+            </div>
+            <div className="contact--content">
+                <a href={gmail}  className="mail-icon">
+                    <img src={mail} width="25px"></img>
+                </a> 
+            </div>
+            <div className="contact--content">
+                <a href={number}  className="call-icon">
+                    <img src={call} width="25px"></img>
+                </a> 
+            </div>
+            <div className="contact--content">
+                <a href={instagram}  className="insta-icon">
+                    <img src={insta} width="25px"></img> 
+                </a> 
+            </div>
         </div>
     )
 }
-
-
-// inside div -> logo + info
