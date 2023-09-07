1c59c0dec92cbd3b853b85413317bc03db53f464
Parth <parthp15@yahoo.com>
2023-09-07 15:43:13 -0700

    removed unnecesary files

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -16,37 +16,3 @@ export default function App() {
         </div>
     )
 }
-
-// import Contacts from 'react-native-contacts';
-
-// export default function SavePhoneNumberButton() {
-//   const handleSavePhoneNumber = async () => {
-//     try {
-//       // Request permission if not already granted
-//       const granted = await Contacts.requestPermission();
-      
-//       if (granted === 'authorized') {
-//         // Create a new contact
-//         const newContact = {
-//           phoneNumbers: [
-//             {
-//               label: 'mobile',
-//               number: '+1 (236) 995-2972',
-//             },
-//           ],
-//         };
-//         await Contacts.addContact(newContact);
-//         console.log('Phone number saved successfully.');
-//       } else {
-//         console.log('Permission denied.');
-//       }
-//     } catch (error) {
-//       console.error('Error saving phone number:', error);
-//     }
-//   }
-  
-//   return (
-//     <button onClick={handleSavePhoneNumber}>Save Phone Number</button>
-//   );
-// }
-  
